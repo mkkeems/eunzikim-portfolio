600bf697707ec203069746b82aa475f9fbd98a7f
Michelle Kim <mkkeems@gmail.com>
2021-06-10 00:01:16 +0900

    footer border styled

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,17 +6,22 @@ import { colors } from "../assets/styles/variables";
 const Wrap = styled.div`
   border-top: 1px solid ${colors.darkGrey};
   display: flex;
-  padding: ${getvw(20)} 0;
+  /* padding: ${getvw(20)} 0; */
   justify-content: space-around;
   flex-shrink: 0;
 
   .footer-content {
+    width: 100%;
     display: flex;
     flex-direction: column;
+    text-align: center;
     font-size: ${getvw(18)};
     font-weight: 500;
-    margin-top: ${getvw(36)};
-    margin-bottom: ${getvw(24)};
+    padding-top: ${getvw(36)};
+    padding-bottom: ${getvw(24)};
+    margin: 0 ${getvw(306)};
+    border-right: 1px solid ${colors.darkGrey};
+    border-left: 1px solid ${colors.darkGrey};
   }
 `;
 
